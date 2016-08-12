c871f1f9ada296e26b181045416233649d79e8a4
Adam Luchini <aluchin@gmail.com>
2016-08-12 10:19:26 -0700

    Adds food-item component

---
+++ app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from 'angular2/core'
+
+export class AppComponent {
+
+}
+
+@Component({
+  selector: 'my-app',
+  template: `
+  <div class="container">
+    <h2>Meal Items</h2>
+  </div>
+    <div>
+      <h3>
+// food goes here
+      </h3>
+
+    </div>
+  `
+})
---
+++ app/food-item-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from 'angular2/core';
+
+@Component ({
+  selector: 'food-item-list',
+  inputs: ['foodItemList'],
+  template: '
+
+  '
+})
+
+export class FoodItemListComponent{
+  
+}
